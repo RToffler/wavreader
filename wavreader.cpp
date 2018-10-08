cac2f7fe58c78c549a03e41ac710d7bcde6223da
Richard Miller <miller.richard41@gmail.com>
2018-10-08 11:06:02 -0400

    initial push

---
+++ wavreader.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+typedef struct RIFFdescriptor{
+    char chunkID[4];
+    long chunkSize;
+};
+
+typedef struct fmtchunk{
+
+};
+
+int main(){
+
+}
